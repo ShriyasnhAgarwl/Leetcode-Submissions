9a8f9d2aed78ac0539702d9b27c3d52e1b4bec9f
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2022-06-08 08:48:45 +0530

    Time: 3 ms (39.03%), Space: 6.2 MB (78.83%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(isPalindrome(s))
+            return 1;
+        else return 2;
+    }
+    bool isPalindrome(string s)
+    {
+        int n=s.size();
+        for(int i =0;i<n/2;i++)
+        {
+            if(s[i]!= s[n-i-1])
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
