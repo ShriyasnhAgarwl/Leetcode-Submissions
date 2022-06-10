733bee9775d4c1611acaf1c9a5c8f0e78e3a553c
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2022-06-10 14:19:58 +0530

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.cpp
+++ Key Pair - GFG/key-pair.cpp
@@ -11,6 +11,7 @@ public:
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
 	    unordered_map<int,int> m;
 	    for(int i=0;i<n;i++)
 	    {
