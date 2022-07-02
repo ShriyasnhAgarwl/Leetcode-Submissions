65010ae996a13eecf3f581f4f5ebb00bfa27a2ae
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2022-07-02 09:27:05 +0530

    Time: 5 ms (64.30%), Space: 9.6 MB (51.71%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,16 +1,37 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         for(int i=0 ; i<matrix.size();i++)
+//         {
+//             for(int j=0;j<matrix[0].size();j++)
+//             {
+//                 if(matrix[i][j]==target)
+//                 {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0 ; i<matrix.size();i++)
+        int rowsize =  matrix.size(),
+            colsize = matrix[0].size(),
+            row=0 , col=colsize-1;
+        
+        while(row<rowsize && col>-1)
         {
-            for(int j=0;j<matrix[0].size();j++)
-            {
-                if(matrix[i][j]==target)
-                {
-                    return true;
-                }
-            }
+            int curr=matrix[row][col];
+            if(curr==target) return true;
+            if(target>curr) row++;
+            else col--;
         }
+
+        
         return false;
     }
 };
+
