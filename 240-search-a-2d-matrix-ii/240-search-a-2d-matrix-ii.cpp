ccc9c6b0138848c2579d7958fdf93870176333a4
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2022-07-24 10:37:42 +0530

    Time: 272 ms (21.24%), Space: 14.9 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,21 +1,50 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n1=matrix.size(), n2=matrix[0].size();
-        int i =n1-1, j=0;
-        while(i>=0 && j<n2)
+        // int n1=matrix.size(), n2=matrix[0].size();
+        // int i =n1-1, j=0;
+        // while(i>=0 && j<n2)
+        // {
+        //     if(matrix[i][j] == target)
+        //     {
+        //         return true;
+        //     }
+        //     else if(matrix[i][j]> target )
+        //     {
+        //         i--;
+        //     }
+        //     else
+        //     {
+        //         j++;
+        //     }
+        // }
+        // return false;
+        
+        int m=matrix.size(), n=matrix[0].size();
+        int low, high, mid;
+        for(int i =0;i<m;i++)
         {
-            if(matrix[i][j] == target)
+            if(matrix[i][0]<=target && matrix[i][n-1]>=target)
             {
-                return true;
-            }
-            else if(matrix[i][j]> target )
-            {
-                i--;
-            }
-            else
-            {
-                j++;
+                low=0;
+                high=n;
+                
+                while(low<high)
+                {
+                    mid=(low+high)/2;
+                    if(matrix[i][mid] == target)
+                    {
+                        return true;
+                    }
+                    else if( matrix[i][mid]<target)
+                    {
+                        low=mid+1;
+                    }
+                    else
+                    {
+                      high=mid;   
+                    }
+                }
             }
         }
         return false;
