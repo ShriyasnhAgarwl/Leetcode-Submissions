0240e48f9f6c5d04a74b5220028a9682311ee4b6
SHRIYANSH AGARWAL <shriyanshagar9026@gmail.com>
2022-09-13 22:37:15 +0530

    Time: 31 ms (19.95%), Space: 13.8 MB (91.77%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int n = data.size();
+        int count=0;
+        for(int i=0 ;i<n; i++)
+        {
+            int x=data[i];
+            if(!count)
+            {
+                if(x>>5 == 0b110)
+                {
+                    count=1;
+                }
+                else if(x>>4 == 0b1110)
+                {
+                    count=2;
+                }
+                else if(x>>3 == 0b11110)
+                {
+                    count=3;
+                }
+                else if(x>>7)
+                {
+                    return false;
+                }
+                
+            }
+            else
+            {
+                if(x>>6 != 0b10)
+                    return false;
+                count--;
+            }
+        }
+        return (count == 0);
+    }
+};
